acdfa2372bd0ee8f977fe654833a9acc4d9e009c
HiImChobo <matzukhe@gmail.com>
2021-09-16 03:38:27 -0500

    My first PHP try

---
+++ index.php
@@ -0,0 +1,129 @@
+<html>
+    <head>
+        <title>PHP Tutorial</title>
+    </head>
+    <body>
+        <?php
+            // $nombre = "Henry";
+
+            // echo "Mi nombre es " . $nombre;
+
+            // Integer float String Boolean array Object
+
+            // define("PI", 3.14159);
+
+            // echo "El valor de PI es " . PI;
+
+            // echo "6 + 4 = ". (6 + 4) . "<br/>";
+            // echo "6 - 4 = ". (6 - 4) . "<br/>";
+            // echo "6 * 4 = ". (6 * 4) . "<br/>";
+            // echo "6 / 4 = ". (6 / 4) . "<br/>";
+            // echo "6 % 4 = ". (6 % 4) . "<br/>";
+
+            // $minumero = 6;
+            // $refnumero = &$minumero;
+            // $minumero = 20;
+            // echo $refnumero;
+
+            // $miedad = 21;
+            // if($miedad>30){
+            //     echo "Mi edad es mayor a 10.";
+
+            // }else if($miedad ===21){
+            //     echo "Tu edad es 21";
+            // } else{
+            //     echo "No puedo adivinar tu edad.";
+            // }
+
+            // if (($miedad === 21) && ($miedad < 25)){
+            //     echo "Tu edad es 21";
+            // }
+
+            // if (($miedad === 21) || ($miedad < 25)){
+            //     echo "Tu edad es 21";
+            // }
+
+            // $minumero = 20;
+            // $resultado = ($minumero < 40) ? "Tu numero es menor a 40" : "Tu numero no es mayor a 40"; 
+            // echo $resultado;  
+
+            // $usuario = "Henry";
+            // switch($usuario){
+            //     case "Luis":
+            //         echo "Hola Luis";
+            //         break;
+            
+            //     case "Henry":
+            //         echo "Hola Henry";
+            //         break;
+
+            //     case "Carmen":
+            //         echo "Hola Carmen";
+            //         break;
+
+            //     default:
+            //         echo "No puedo adivinar tu nombre";
+            //         break;
+            // }
+
+            $minumero = 0;
+
+            // while($minumero < 10){
+            //     echo $minumero;
+            //     $minumero++;
+            // }
+
+            // for($num = 1; $num <= 10; $num++){
+            //     echo $num . "<br/>";
+            // }
+
+            // for($num = 1; $num <= 10; $num++){
+            //     echo $num;
+            //     if($num !== 10){
+            //         echo ", ";
+            //     }else{
+            //         break;
+            //     }
+            // }
+
+            // $personas = array("Henry", "Luis", "Carmen", "Beto");
+            // print_r($personas);
+
+            // foreach($personas as $persona){
+            //     echo $persona . "<br/>";
+            // }
+
+            // $personas = array("Luis" => 20, "Beto" => "30", "Carmen" => 20);
+            // foreach($personas as $key => $val){
+            //     // echo $key . " tiene ". $val . "<br/>";
+            //     // echo "$key tiene $val <br/>";
+            // }
+
+            // $cadena = "          Los colores   ";
+            // echo strlen($cadena). "<br/>";
+            // echo strlen(ltrim($cadena)). "<br/>";
+            // echo strlen(rtrim($cadena)). "<br/>";
+            // echo strlen(trim($cadena)) . "<br/>";
+            
+            // $usuario = "Henry";
+            // printf("Mi Nombre es %s", $usuario)
+
+            // $mensaje = "Los cuadernos";
+
+            // echo strtoupper($mensaje). "<br/>";
+            // echo strtolower($mensaje). "<br/>";
+            
+            // print_r(explode(" ", $mensaje));
+
+            // function myFunction(){
+            //     return "Henry";
+            // }
+            // echo "Mi nombre es " . myFunction() . "<br/>";
+
+            // function myFunctionA($a, $b){
+            //     return $a + $b;
+            // }
+            // echo "La suma de 6 + 4 =" . myFunctionA(6, 4);
+        ?>
+    </body>
+</html>
